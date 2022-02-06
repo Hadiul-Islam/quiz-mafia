aa9635eff5a449b18cbead016e945158b1d7461f
hadiul-islam <hadiulislamofficial@gmail.com>
2022-02-06 20:00:45 +0600

    add firebase auth

--- src/components/App.js
+++ src/components/App.js
@@ -6,15 +6,18 @@ import Signup from './pages/Signup';
 import Login from './pages/Login';
 import Quiz from './pages/Quiz';
 import Result from './pages/Result';
+import { AuthProvider } from '../contexts/AuthContext';
 
 const App = () => {
   return (
     <Routes>
-        <Route path="/" element={ <Home /> } />
-        <Route path="/signup" element={ <Signup /> } />
-        <Route path="/login" element={ <Login /> } />
-        <Route path="/quiz" element={ <Quiz /> } />
-        <Route path="/result" element={ <Result /> } />
+      <AuthProvider>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/result" element={<Result />} />
+      </AuthProvider>
     </Routes>
   );
 };
